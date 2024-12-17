d70c54f4687332a93d0abd5fc36cf66a0d188e6f
nameishyam <geddamgowtham04@outlook.com>
2024-12-17 09:26:44 +0530

    tasks page

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import Cookie from "js-cookie";
 
 const Tasks = ({ tasks }) => {
-  console.log(typeof tasks);
+  const navigate = useNavigate();
+
+  const navigateToTodoTask = (taskId, taskName) => {
+    Cookie.set("taskId", taskId);
+    Cookie.set("taskName", taskName);
+    navigate(`/tasks`);
+  };
+
   return (
     <>
       <ul className="text-white text-lg">
@@ -9,7 +18,7 @@ const Tasks = ({ tasks }) => {
           <li
             key={task.id}
             className="py-2 px-4 bg-gray-700 rounded-md mb-2 cursor-pointer hover:bg-gray-600 transition duration-300"
-            onClick={() => navigateToTodoTask(task.id)}
+            onClick={() => navigateToTodoTask(task.id, task.name)}
           >
             {task.name}
           </li>
